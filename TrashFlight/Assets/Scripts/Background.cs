f3e14a70883ab7d2030cda0a256c822f536ba788
양민석 <kurooru98@gmail.com>
2024-07-22 20:34:04 +0900

    7/22 배경 만들기, 움직이는 배경 만들기

--- TrashFlight/Assets/Scripts/Background.cs
+++ TrashFlight/Assets/Scripts/Background.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // private 선언 -> 다른 클래스에서 사용 불가능
+    private float moveSpeed = 3f; // 실수 선언 f붙여줌
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() // 컴퓨터 성능에 따라 불리는 횟수가 달라짐(Frame Per Second)
+    {   
+        // Time.deltaTime -> 컴퓨터 성능에 따른 실행 횟수 차이 방지
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime; 
+        if (transform.position.y < -10) {
+            // y값을 -10에서 10으로 옮기기 => 10 * 2
+            transform.position += new Vector3(0, 10 * 2f, 0);
+        }
     }
 }
