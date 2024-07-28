6a06f93159b0cc0d81f98a372ded29e265c206ae
양민석 <kurooru98@gmail.com>
2024-07-28 15:51:42 +0900

    7/28 마우스로 움직이기

--- TrashFlight/Assets/Scripts/Player.cs
+++ TrashFlight/Assets/Scripts/Player.cs
@@ -22,15 +22,26 @@ public class Player : MonoBehaviour
         // Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f);
         // transform.position += moveTo * moveSpeed * Time.deltaTime;
 
-        // moveTo -> 좌우 이동만 허용
-        Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
-        // 키보드로부터 받은 입력값이 좌이동이면
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            // 포지션을 -= moveTo로 변환
-            transform.position -= moveTo;
-        } else if (Input.GetKey(KeyCode.RightArrow)) { // 우이동이면
-            // 포지션을 += moveTo로 변환
-            transform.position += moveTo;
-        }
+        // // moveTo -> 좌우 이동만 허용
+        // Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        // // 키보드로부터 받은 입력값이 좌이동이면
+        // if (Input.GetKey(KeyCode.LeftArrow)) {
+        //     // 포지션을 -= moveTo로 변환
+        //     transform.position -= moveTo;
+        // } else if (Input.GetKey(KeyCode.RightArrow)) { // 우이동이면
+        //     // 포지션을 += moveTo로 변환
+        //     transform.position += moveTo;
+        // }
+
+        // 마우스 좌표 값 가져오기
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        // 범위 못벗어나게 처리
+        // Mathf.Clamp(value, 최솟값, 최댓값)
+        float toX = Mathf.Clamp(mousePos.x, -2.35f, 2.35f);
+
+        // 움직이기
+        transform.position = new Vector3(toX, transform.position.y, transform.position.z);
+
     }
 }
