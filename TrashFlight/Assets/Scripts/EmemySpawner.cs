fb2927b0b8ea873bae09f7e25fd5c87b7fafdee4
양민석 <kurooru98@gmail.com>
2024-08-03 11:29:59 +0900

    8/3 랜덤으로 적 만들기 (전반전)

---
+++ TrashFlight/Assets/Scripts/EmemySpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmemySpawner : MonoBehaviour
+{   
+    // 적 저장
+    [SerializeField]
+    private GameObject[] ememies;
+
+    // x좌표 저장 (5개의 적의 시작 x좌표)
+    private float[] arrPosX = {-2.2f, -1.1f, 0f, 1.1f, 2.2f};
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        // 랜덤으로 인덱스 생성하기
+        int index = Random.Range(0, ememies.Length);
+
+        // 생성하기
+        SpawnEnemy(arrPosX[0], index);
+    }
+
+    // 적 생성 함수
+    void SpawnEnemy(float posX, int index) {
+        // 적 위치 정의
+        Vector3 spawnPos = new Vector3(posX, transform.position.y, transform.position.z);
+
+        // 적 객체 정의
+        Instantiate(ememies[index], spawnPos, Quaternion.identity);
+    }
+}
