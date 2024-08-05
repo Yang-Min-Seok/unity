d792d2d087d48ff03b7f57b4b585018f765d8fca
양민석 <kurooru98@gmail.com>
2024-08-05 20:40:45 +0900

    8/5 적 무한 생성하기 (전반전)

--- TrashFlight/Assets/Scripts/EmemySpawner.cs
+++ TrashFlight/Assets/Scripts/EmemySpawner.cs
@@ -11,15 +11,33 @@ public class EmemySpawner : MonoBehaviour
     // x좌표 저장 (5개의 적의 시작 x좌표)
     private float[] arrPosX = {-2.2f, -1.1f, 0f, 1.1f, 2.2f};
 
+    // 적 생성 간격
+    [SerializeField]
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {   
-        // arrPosX에서 posX를 하나씩 꺼내 반복
-        foreach(float posX in arrPosX) {
-            // 랜덤으로 인덱스 생성하기
-            int index = Random.Range(0, ememies.Length);
-            // 생성하기
-            SpawnEnemy(posX, index);
+        StartEnemyRoutine();
+    }
+
+    void StartEnemyRoutine() {
+        StartCoroutine("EnemyRoutine");
+    }
+
+    // 적 무한 생성하기
+    IEnumerator EnemyRoutine() {
+        yield return new WaitForSeconds(3f);
+        while (true) {
+            // arrPosX에서 posX를 하나씩 꺼내 반복
+            foreach(float posX in arrPosX) {
+                // 랜덤으로 인덱스 생성하기
+                int index = Random.Range(0, ememies.Length);
+                // 생성하기
+                SpawnEnemy(posX, index);
+            }
+            // 
+            yield return new WaitForSeconds(spawnInterval);
         }
     }
 
