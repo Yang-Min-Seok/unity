4fdaf1ffe00ba1acfc7e42c53045f1d025a689eb
양민석 <kurooru98@gmail.com>
2024-08-05 20:52:20 +0900

    8/5 적 무한 생성하기 (후반전)

--- TrashFlight/Assets/Scripts/EmemySpawner.cs
+++ TrashFlight/Assets/Scripts/EmemySpawner.cs
@@ -28,23 +28,45 @@ public class EmemySpawner : MonoBehaviour
     // 적 무한 생성하기
     IEnumerator EnemyRoutine() {
         yield return new WaitForSeconds(3f);
+
+        // 생성횟수
+        int spawnCount = 0;
+        // 생성할 적의 인덱스
+        int enemyIndex = 0;
+
         while (true) {
             // arrPosX에서 posX를 하나씩 꺼내 반복
             foreach(float posX in arrPosX) {
-                // 랜덤으로 인덱스 생성하기
-                int index = Random.Range(0, ememies.Length);
                 // 생성하기
-                SpawnEnemy(posX, index);
+                SpawnEnemy(posX, enemyIndex);
             }
-            // 
+
+            spawnCount++;
+
+            if (spawnCount % 10 == 0) {
+                enemyIndex++;
+            }
+            
             yield return new WaitForSeconds(spawnInterval);
         }
     }
 
     // 적 생성 함수
     void SpawnEnemy(float posX, int index) {
+        
         // 적 위치 정의
         Vector3 spawnPos = new Vector3(posX, transform.position.y, transform.position.z);
+
+        // 중간중간 섞어주기
+        if (Random.Range(0, 5) == 0) {
+            index++;
+        }
+
+
+        // 인덱스 오버 방지
+        if (index >= ememies.Length) {
+            index = ememies.Length - 1;
+        }
 
         // 적 객체 정의
         Instantiate(ememies[index], spawnPos, Quaternion.identity);
