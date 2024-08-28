843cbbe17b8209962e2ace20b08e5124b8a6f107
양민석 <kurooru98@gmail.com>
2024-08-28 21:35:00 +0900

    8/28 결과 화면, 마무리

--- TrashFlight/Assets/Scripts/GameManager.cs
+++ TrashFlight/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro; // TextMeshPro 사용 위함
+using UnityEngine.SceneManagement; // Scene 사용 위함
 
 public class GameManager : MonoBehaviour
 {   
@@ -9,6 +10,11 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI text;
+
+    // GameOver Panel
+    [SerializeField]
+    private GameObject gameOverPanel;
+
     private int coin = 0;
 
     // public이여도 인스펙터에서 확인 불가능
@@ -32,7 +38,7 @@ public class GameManager : MonoBehaviour
         // UI 반영
         text.SetText(coin.ToString());
 
-        if (coin % 10 == 0) {
+        if (coin % 30 == 0) {
             // player 객체 불러오기
             Player player = FindObjectOfType<Player>();
             // 무기 업그레이드
@@ -46,12 +52,27 @@ public class GameManager : MonoBehaviour
     public void SetGameOver() {
         // 게임 종료 처리
         isGameOVer = true;
+        
         // ememySpawner 찾기
         EmemySpawner enemySpawner = FindObjectOfType<EmemySpawner>();
+        
         // 찾았으면
         if (enemySpawner != null) {
             // 생성 종료
             enemySpawner.StopEmemyRoutine();
         }
+
+        // 1초 후 게임 종료 패널 활성화
+        Invoke("ShowGameOverPanel", 1f);
+    }
+
+    // 게임 종료 패널 활성화
+    void ShowGameOverPanel() {
+        gameOverPanel.SetActive(true);
+    }
+
+    // 다시 시작
+    public void PlayAgain() {
+        SceneManager.LoadScene("SampleScene");
     }
 }
